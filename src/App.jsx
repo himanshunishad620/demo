a3594b02d561bc889149a75b850071864e77e227
himanshunishad620 <himanshunishad620@gmail.com>
2025-07-03 08:42:36 +0530

    scroolbar hidden

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { ToastContainer } from "react-toastify";
 
 export default function App() {
   return (
-    <div className="w-full md:w-3/5 m-auto ">
+    <div className=" overflow-auto h-screen scrollbar-hidden w-full md:w-3/5 m-auto ">
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/:id" element={<BookCard />} />
